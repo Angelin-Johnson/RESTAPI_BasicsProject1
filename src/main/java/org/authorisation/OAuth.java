12eba5f70b1b6ad01ec08e3390ac3aa519b561d0
Angelin Johnson <angelinjohnson.comp@gmail.com>
2024-07-24 09:25:44 +0100

    Initial commit

--- src/main/java/org/authorisation/OAuth.java
+++ src/main/java/org/authorisation/OAuth.java
@@ -4,7 +4,7 @@ import static io.restassured.RestAssured.given;
 
 public class OAuth {
     public static void main(String... args){
-        given().auth().oauth2("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")
+        given().auth().oauth2("")
                 .when().get(" https://api.github.com/user/repos")
                 .then().log().all()
                 .assertThat()
